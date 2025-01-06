5973ea24554c4f7b604b42609e1211201d5885be
Guy-HKV <75747556+guydup@users.noreply.github.com>
2025-01-06 16:39:07 +0100

    enable tests

---
+++ test/test_probabilisticpiping.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from probabilistic_piping import ProbInput, ProbPipingFixedWaterlevelSimple
+
+
+def test_full_fragilitycurve():
+    df_path = Path(__file__).parent / "data" / "full_test.xlsx"
+    with pd.ExcelFile(df_path) as xlsx:
+        df_input = pd.read_excel(xlsx, sheet_name="input", index_col=0, header=0)
+        df_expected_results = pd.read_excel(
+            xlsx, sheet_name="output", index_col=None, header=0
+        )
+
+    # Create probabilistic piping object and calculate fragility curve
+    prob = ProbPipingFixedWaterlevelSimple(progress=False)
+    inp_form = ProbInput.from_dataframe(df_input)
+    _, pu, ph, pp, pc = prob.fixed_waterlevel_fragilitycurve(inp_form)
+
+    # Create results dataframe similar to the expected results
+    df_results = {
+        "waterstand": [],
+        "prob_uplift": [],
+        "prob_heave": [],
+        "prob_sellmeijer": [],
+        "mechanisme": [],
+        "prob_cond": [],
+    }
+    for ru, rh, rp, rc in zip(pu.results, ph.results, pp.results, pc.results):
+        df_results["waterstand"].append(ru.h)
+        df_results["prob_uplift"].append(ru.prob_cond)
+        df_results["prob_heave"].append(rh.prob_cond)
+        df_results["prob_sellmeijer"].append(rp.prob_cond)
+        df_results["mechanisme"].append(rc.mechanism)
+        df_results["prob_cond"].append(rc.prob_cond)
+    df_results = pd.DataFrame(df_results)
+
+    # Compare the results column by column
+    for name in df_results.columns:
+        if name == "mechanisme":
+            col_equal = df_results[name].to_numpy() == df_expected_results[name].to_numpy()
+            assert col_equal.all()
+        else:
+            assert np.allclose(
+                df_results[name].to_numpy(),
+                df_expected_results[name].to_numpy(),
+                rtol=1e-8,
+                atol=1e-8,
+            )
